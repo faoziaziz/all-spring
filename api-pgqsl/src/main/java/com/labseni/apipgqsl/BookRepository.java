ff7f480873b43d5c115be233358642c9ebaf93a1
faoziaziz <admin@labseni.com>
2020-03-02 00:18:30 +0700

    nambahin

---
+++ api-pgqsl/src/main/java/com/labseni/apipgqsl/BookRepository.java
@@ -0,0 +1,9 @@
+package com.labseni.apipgqsl;
+
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface BookRepository extends CrudRepository<Book, Long>{
+    List<Book> findByName(String name);
+}
