096f4d15f2e45b88d766c8fd9f5c4d88ea4445e4
faoziaziz <admin@labseni.com>
2020-03-01 08:37:15 +0700

    add spring development kit

---
+++ api-pgqsl/src/main/java/com/labseni/apipgqsl/ApiPgqslApplication.java
@@ -0,0 +1,13 @@
+package com.labseni.apipgqsl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiPgqslApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiPgqslApplication.class, args);
+	}
+
+}
---
+++ api-pgqsl/src/test/java/com/labseni/apipgqsl/ApiPgqslApplicationTests.java
@@ -0,0 +1,13 @@
+package com.labseni.apipgqsl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiPgqslApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
